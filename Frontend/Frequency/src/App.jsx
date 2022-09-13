b7129b637e1fba76e35433156f59dd6cb099e438
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2022-09-13 15:13:24 +0530

    Building Login Page

--- Frontend/Frequency/src/App.jsx
+++ Frontend/Frequency/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 // import './App.css'
+import AuthContextProvider from './Contexts/AuthContext'
 import AppRouter from './Components/AppRouter'
 
 function App(props) {
   return (
-    <AppRouter />
+    <AuthContextProvider>
+      <AppRouter />
+    </AuthContextProvider>
   )
 }
 
